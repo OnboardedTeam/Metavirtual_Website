cb37d024a2afc1a6cd7a387570b07da6d752d1be
Martin <61696199+NitramO-YT@users.noreply.github.com>
2023-06-23 23:44:29 +0200

    Update mobile vers. & custom cursor - 23/06/2023 23h40

--- customCursor.js
+++ customCursor.js
@@ -7,4 +7,29 @@ window.addEventListener('mousemove', function (e) {
         customCursor.style.left = (e.clientX / windowWidth * 100) + '%';
         customCursor.style.top = (e.clientY / windowHeight * 100) + '%';
     });
+
+    showCursor();
 });
+
+//hide cursor on losing page focus
+window.addEventListener("blur", (event) => {
+    setTimeout(() => {
+        if (document.hidden) {
+            hideCursor();
+        }
+    }, 100);
+});
+
+//hide cursor on losing page focus
+window.addEventListener("mouseout", (event) => {
+    hideCursor();
+});
+
+function hideCursor() {
+    var customCursor = document.querySelector('.custom-cursor');
+    customCursor.setAttribute("outPage", "");
+}
+function showCursor() {
+    var customCursor = document.querySelector('.custom-cursor');
+    customCursor.removeAttribute("outPage");
+}
